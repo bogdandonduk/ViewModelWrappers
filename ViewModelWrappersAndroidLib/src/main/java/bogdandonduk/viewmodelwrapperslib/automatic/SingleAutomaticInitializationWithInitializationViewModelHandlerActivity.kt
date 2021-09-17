4dd0a1c735d99e0b228951f0280fcb5ba63cae88
bogdandonduk <bogdan.donduk@gmail.com>
2021-09-17 15:04:32 +0700

    Added activity reference to initialization lambda SingleAutomaticInitializationWithInitializationViewModelHandlerActivity

--- ViewModelWrappersAndroidLib/src/main/java/bogdandonduk/viewmodelwrapperslib/automatic/SingleAutomaticInitializationWithInitializationViewModelHandlerActivity.kt
+++ ViewModelWrappersAndroidLib/src/main/java/bogdandonduk/viewmodelwrapperslib/automatic/SingleAutomaticInitializationWithInitializationViewModelHandlerActivity.kt
@@ -9,16 +9,16 @@ import androidx.lifecycle.ViewModelStoreOwner
 import bogdandonduk.viewmodelwrapperslib.core.GenericViewModelFactory
 
 interface SingleAutomaticInitializationWithInitializationViewModelHandlerActivity<T : ViewModel> {
-    var viewModelInitialization: (intent: Intent) -> T
+    var viewModelInitialization: (activity: Activity, intent: Intent) -> T
 
     fun getInitializedViewModel(activity: Activity, viewModelStoreOwner: ViewModelStoreOwner) : T {
-        val viewModel = viewModelInitialization.invoke(activity.intent)
+        val viewModel = viewModelInitialization.invoke(activity, activity.intent)
 
         return ViewModelProvider(viewModelStoreOwner, GenericViewModelFactory(viewModel)).get(viewModel::class.java)
     }
 
     fun getInitializedViewModel(activity: Activity, viewModelStore: ViewModelStore) : T {
-        val viewModel = viewModelInitialization.invoke(activity.intent)
+        val viewModel = viewModelInitialization.invoke(activity, activity.intent)
 
         return ViewModelProvider(viewModelStore, GenericViewModelFactory(viewModel)).get(viewModel::class.java)
     }
