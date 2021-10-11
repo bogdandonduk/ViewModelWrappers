2b63ddfac4a31a8dece2635d0d095130d1e0f921
bogdandonduk <bogdan.donduk@gmail.com>
2021-10-11 19:23:52 +0700

    Added overload with simplified signature for getInitializedViewModel() method's signature in SingleAutomaticInitializationWithInitializationViewModelHandlerActivity (without viewModelStore parameter)

--- ViewModelWrappersAndroidLib/src/main/java/bogdandonduk/viewmodelwrapperslib/automatic/SingleAutomaticInitializationWithInitializationViewModelHandlerActivity.kt
+++ ViewModelWrappersAndroidLib/src/main/java/bogdandonduk/viewmodelwrapperslib/automatic/SingleAutomaticInitializationWithInitializationViewModelHandlerActivity.kt
@@ -2,6 +2,7 @@ package bogdandonduk.viewmodelwrapperslib.automatic
 
 import android.app.Activity
 import android.content.Intent
+import androidx.activity.ComponentActivity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStore
@@ -11,15 +12,21 @@ import bogdandonduk.viewmodelwrapperslib.core.GenericViewModelFactory
 interface SingleAutomaticInitializationWithInitializationViewModelHandlerActivity<T : ViewModel> {
     var viewModelInitialization: (activity: Activity) -> T
 
-    fun getInitializedViewModel(activity: Activity, viewModelStoreOwner: ViewModelStoreOwner) : T {
+    fun getInitializedViewModel(activity: ComponentActivity) : T {
         val viewModel = viewModelInitialization.invoke(activity)
 
-        return ViewModelProvider(viewModelStoreOwner, GenericViewModelFactory(viewModel)).get(viewModel::class.java)
+        return ViewModelProvider(activity, GenericViewModelFactory(viewModel)).get(viewModel::class.java)
     }
 
     fun getInitializedViewModel(activity: Activity, viewModelStore: ViewModelStore) : T {
         val viewModel = viewModelInitialization.invoke(activity)
 
         return ViewModelProvider(viewModelStore, GenericViewModelFactory(viewModel)).get(viewModel::class.java)
+    }
+
+    fun getInitializedViewModel(activity: Activity, viewModelStoreOwner: ViewModelStoreOwner) : T {
+        val viewModel = viewModelInitialization.invoke(activity)
+
+        return ViewModelProvider(viewModelStoreOwner, GenericViewModelFactory(viewModel)).get(viewModel::class.java)
     }
 }
